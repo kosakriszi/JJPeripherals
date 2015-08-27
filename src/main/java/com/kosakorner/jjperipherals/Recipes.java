97e808696408331880f95770cba3d937872bbc9b
kosakriszi <kosakriszi@gmail.com>
2015-08-27 15:53:08 -0400

    fix recipe maybe?

--- src/main/java/com/kosakorner/jjperipherals/Recipes.java
+++ src/main/java/com/kosakorner/jjperipherals/Recipes.java
@@ -1,21 +1,15 @@
 package com.kosakorner.jjperipherals;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.crafting.CraftingManager;
-import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.oredict.ShapedOreRecipe;
-
-import java.util.List;
 
 public class Recipes {
 
     public static void register() {
-        @SuppressWarnings("unchecked")
-        final List<IRecipe> recipeList = CraftingManager.getInstance().getRecipeList();
-
         if (JJPeripherals.Blocks.peripheralRelay != null) {
-            recipeList.add(new ShapedOreRecipe(JJPeripherals.Blocks.peripheralRelay, "srs", "sss", "sgs", 's', Blocks.stone, 'g', Items.gold_ingot, 'r', Items.redstone));
+            GameRegistry.addRecipe(new ShapedOreRecipe(JJPeripherals.Blocks.peripheralRelay, "srs", "sss", "sgs", 's', Blocks.stone, 'g', Items.gold_ingot, 'r', Items.redstone));
         }
 
     }
