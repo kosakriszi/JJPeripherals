194e67663156798ffec903a154cf08c7c2536852
kosakriszi <kosakriszi@gmail.com>
2015-08-28 22:53:14 -0400

    bumped version so i can rip my hair out

--- src/main/java/com/kosakorner/jjperipherals/JJPeripherals.java
+++ src/main/java/com/kosakorner/jjperipherals/JJPeripherals.java
@@ -32,7 +32,7 @@ public class JJPeripherals {
 
     public static final String MODID = "jjperipherals";
     public static final String DISPLAY = "JJPeripherals";
-    public static final String VERSION = "1.0.5";
+    public static final String VERSION = "1.0.6";
 
     @Mod.Instance(MODID)
     public static JJPeripherals instance;
